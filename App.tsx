3ae37fe8893e89d44f84cbaaf57dad424ee198df
t0m0120 <highwest.t@gmail.com>
2019-07-17 17:46:01 +0900

    react-native-modalbox

--- App.tsx
+++ App.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useRef} from 'react';
+import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import Modal from 'react-native-modalbox';
 
 export default function App() {
+  const inputRef = useRef(null);
+  const i = 4
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <TouchableOpacity onPress={() => inputRef.current.open()} style={styles.btn} ><Text>test</Text></TouchableOpacity>
+      <Modal style={{ height: 30 + 80 * i, justifyContent: 'flex-start', alignItems: 'flex-start' }} position={"bottom"} ref={inputRef}>
+        <View style={{height: 30, width: '100%', backgroundColor: 'red'}} />
+        <TouchableOpacity style={{height: 80, borderBottomColor: '#ccc', borderBottomWidth: 1,}}><Text>Modal on bottom with backdrop</Text></TouchableOpacity>
+        <TouchableOpacity style={{height: 80, borderBottomColor: '#ccc', borderBottomWidth: 1,}}><Text>Modal on bottom with backdrop</Text></TouchableOpacity>
+        <TouchableOpacity style={{height: 80, borderBottomColor: '#ccc', borderBottomWidth: 1,}}><Text>Modal on bottom with backdrop</Text></TouchableOpacity>
+        <TouchableOpacity style={{height: 80, borderBottomColor: '#ccc', borderBottomWidth: 1,}}><Text>Modal on bottom with backdrop</Text></TouchableOpacity>
+        <TouchableOpacity style={{height: 80, borderBottomColor: '#ccc', borderBottomWidth: 1,}}><Text>Modal on bottom with backdrop</Text></TouchableOpacity>
+      </Modal>
     </View>
   );
 }
@@ -16,4 +28,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  modal: {
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start'
+  },
+  btn: {
+    margin: 10,
+    backgroundColor: "#3B5998",
+    color: "white",
+    padding: 10
+  },
+
 });
